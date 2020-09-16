903130f88f44a2af70bbadc0c8e61a44a6bd9c80
Daniel Fordan <danielfordan@me.com>
2020-09-16 16:05:06 +0800

    Begin refactoring

---
+++ components/TrackerTextInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label for="client" class="inputlabel">Client*</label>
+    <input
+      id="client"
+      v-model="newMatter.client"
+      type="text"
+      name="client"
+      placeholder="Client"
+      class="textinput"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    variant: {
+      type: String,
+      default: 'top',
+    },
+  },
+})
+</script>
+
+<style scoped></style>
